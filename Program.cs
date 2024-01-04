95261f3869f3ceddc414030610f0a02832759704
Andreyjesus <andreyjesus07@hotmail.com>
2024-01-04 01:15:52 -0300

    desafio dio.me sistema de hospedagem

--- Program.cs
+++ Program.cs
@@ -6,20 +6,62 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+string[] nomeHosped;
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+Console.WriteLine("Informe a capacidade de hospede");
+int capacidade =  int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o valor da diaria");
+decimal valorDiaria = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe a quantidade de dias a serem reservados");
+int dias = int.Parse(Console.ReadLine());	
+
+Console.WriteLine("Quantos hospedes deseja incluir ??");
+
+int qtdHopsdes = int.Parse(Console.ReadLine());
+
+
+nomeHosped = new string[qtdHopsdes+1];
+
+for (int i = 1; i <= qtdHopsdes; i++)
+{
+	Console.WriteLine($"Digite o nome do {i}º hospede:");
+	nomeHosped[i] = Console.ReadLine();
+}
+
+
+foreach (var nome in nomeHosped)
+{
+	if (nome != null)
+	{
+		Pessoa p1 = new Pessoa(nome: nome);
+		hospedes.Add(p1);
+	}
+		
+}
+
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: capacidade, valorDiaria: valorDiaria);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: dias);
 reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+	
+
+
+try
+{
+	reserva.CadastrarHospedes(hospedes);
+
+	// Exibe a quantidade de hóspedes e o valor da diária
+	Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+	Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+}
+catch (Exception ex)
+{
+
+	Console.WriteLine(ex.Message );
+}
